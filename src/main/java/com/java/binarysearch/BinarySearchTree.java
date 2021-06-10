7d27fe6277d9a2ca4bbc5754880255f111c5f1cc
siva209 <sivanarayana209@gmail.com>
2021-06-10 20:10:25 +0530

    Added:Implementation for search method in binary search tree

--- src/main/java/com/java/binarysearch/BinarySearchTree.java
+++ src/main/java/com/java/binarysearch/BinarySearchTree.java
@@ -1,23 +1,28 @@
 package com.java.binarysearch;
-
 public class BinarySearchTree<E extends Comparable<E>> {
 	private BinaryNode<E> root;
 	public BinarySearchTree(){
 		this.root = null;	
 	}
+	/**
+	 * Usecase 1
+	 * Function to add element onto the tree
+	 * @param data
+	 */
 	public void add(E data) {
 		this.root = this.addNode(root, data); 	
 	}
-	
 	private BinaryNode<E> addNode(BinaryNode<E> current, E data) {
+		// When the node is null create new node
 		if(current == null) {
 			return new BinaryNode<E>(data);
 		}
+		// if data is less than current node key add to left
 		else if(current.key.compareTo(data)>0) {
 			current.leftNode = addNode(current.leftNode, data);
 		}
-		else
-		{
+		// if data is more than current node key add to right
+		else {
 			current.rightNode = addNode(current.rightNode, data);
 		}
 		return current;
@@ -25,9 +30,37 @@ public class BinarySearchTree<E extends Comparable<E>> {
 	public int size() {
 		return this.getSize(root);
 	}
+	// if no root return 0 else return root count->1 + size of left subtree + size of right subtree 
 	public int getSize(BinaryNode<E> current) {
 		return current == null ? 0 : 1 + this.getSize(current.leftNode) + this.getSize(current.rightNode);
 	}
+	//Binary Search Tree is passed as a parameter to search
+	public boolean search(E data) {
+		return searchNode(this.root, data);
+	}
+	/**
+	 * Usecase 3
+	 * Recursive Search takes place
+	 * @param current
+	 * @param data
+	 * @return
+	 */
+	public boolean searchNode(BinaryNode<E> current, E data) {
+		// if root is absent return false
+		if(current == null) {
+			return false;
+		}
+		//if data matches with root return true
+		else if(current.key == data) {
+			return true;
+		}
+		//repeat the search operation for the left subtree
+		else if(current.key.compareTo(data) > 0) {
+			return this.searchNode(current.leftNode, data);
+		}
+		//repeat the operation for right subtree
+		else {
+			return this.searchNode(current.rightNode, data);
+		}
+	}
 }
-
-
--- src/test/java/com/java/test/BinarySearchTreeTest.java
+++ src/test/java/com/java/test/BinarySearchTreeTest.java
@@ -37,6 +37,25 @@ public class BinarySearchTreeTest {
 		binarySearchTree.add(65);
 		binarySearchTree.add(67);
 		assertEquals(13, binarySearchTree.size());
+	}
+	
+	@Test
+	 public void givenABinaryTree_whenSearchedForElement_shouldReturnTrueIfPresent() {
+		BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<Integer>();
+		binarySearchTree.add(56);
+		binarySearchTree.add(30);
+		binarySearchTree.add(70);
+		binarySearchTree.add(22);
+		binarySearchTree.add(40);
+		binarySearchTree.add(60);
+		binarySearchTree.add(95);
+		binarySearchTree.add(3);
+		binarySearchTree.add(11);
+		binarySearchTree.add(63);
+		binarySearchTree.add(16);
+		binarySearchTree.add(65);
+		binarySearchTree.add(67);
+		assertEquals(true, binarySearchTree.search(63));
 	}	
 }
 
