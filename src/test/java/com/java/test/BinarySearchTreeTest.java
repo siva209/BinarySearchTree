961f2990bbe489070fe5274e53f80564a108e45d
siva209 <sivanarayana209@gmail.com>
2021-06-10 20:06:30 +0530

    Added:Multiple elements on to the Binary Search Tree and Tested

--- src/test/java/com/java/test/BinarySearchTreeTest.java
+++ src/test/java/com/java/test/BinarySearchTreeTest.java
@@ -19,5 +19,25 @@ public class BinarySearchTreeTest {
 		binarySearchTree.add(thirdNode.key);
 		assertEquals(3, binarySearchTree.size());
 	}
+	
+	@Test
+	 public void givenThirteenIntegers_whenAdded_shouldReturnSizeAsThirteen() {
+		BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<Integer>();
+		binarySearchTree.add(56);
+		binarySearchTree.add(30);
+		binarySearchTree.add(70);
+		binarySearchTree.add(22);
+		binarySearchTree.add(40);
+		binarySearchTree.add(60);
+		binarySearchTree.add(95);
+		binarySearchTree.add(3);
+		binarySearchTree.add(11);
+		binarySearchTree.add(63);
+		binarySearchTree.add(16);
+		binarySearchTree.add(65);
+		binarySearchTree.add(67);
+		assertEquals(13, binarySearchTree.size());
+	}	
 }
+
 
